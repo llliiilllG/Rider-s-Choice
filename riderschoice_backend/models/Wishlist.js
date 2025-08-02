eead7ef7b52fadf2de34b545649e6bdf6753c9cb
llliiilllG <koiralagaurav22@gmail.com>
2025-08-02 13:35:39 +0545

    Fix wishlist functionality - Update Wishlist model to accept string customer ID

--- riderschoice_backend/models/Wishlist.js
+++ riderschoice_backend/models/Wishlist.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const wishlistSchema = new mongoose.Schema({
     customer: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Customer",
+        type: String, // Changed to String for demo purposes
         required: true,
     },
     packages: [
